b461250d47092e42b90895e6be529a98609984f7
dnxroshan <dilnavasroshan@gmail.com>
2020-06-08 17:24:41 +0530

    SUBJECTS and STANDARDS added

--- constants.py
+++ constants.py
@@ -4,3 +4,18 @@ class UserType():
     Admin = 0
     Examiner = 1
     Candidate = 2
+
+STANDARDS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '+1', '+2']
+
+SUBJECTS = [
+    'Mathematics',
+    'Physics',
+    'Chemistry',
+    'Botony',
+    'Zoology',
+    'Computer Science',
+    'English',
+    'History',
+    'Geography'
+]
+
